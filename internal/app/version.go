ab18ef1876b20469eaf2b3974bc6ca8ba7e24d10
d3mondev <55468528+d3mondev@users.noreply.github.com>
2021-05-03 12:41:01 +0000

    Update sponsors URL

--- internal/app/version.go
+++ internal/app/version.go
@@ -11,5 +11,5 @@ const (
 	AppVersion string = "2.0.0"
 
 	// AppSponsorsURL is the text file containing sponsors information.
-	AppSponsorsURL string = "https://gist.githubusercontent.com/d3mondev/0bfff529a4dad627bdb684ad1ef2506d/raw/7e92b9ddd53c4998863963b2078a82057e37b3e8/sponsors.txt"
+	AppSponsorsURL string = "https://gist.githubusercontent.com/d3mondev/0bfff529a4dad627bdb684ad1ef2506d/raw/sponsors.txt"
 )
