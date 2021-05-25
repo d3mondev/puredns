0a923cb2bffa42bce02f777d610f5d8b684a59b7
d3mondev <55468528+d3mondev@users.noreply.github.com>
2021-05-25 19:17:52 +0000

    Print git branch and revision when using make

--- internal/app/version.go
+++ internal/app/version.go
@@ -8,8 +8,11 @@ const (
 	AppDesc string = "Very accurate massdns resolving and bruteforcing."
 
 	// AppVersion is the program version.
-	AppVersion string = "2.0.1"
+	AppVersion string = "v2.1.0"
 
 	// AppSponsorsURL is the text file containing sponsors information.
 	AppSponsorsURL string = "https://gist.githubusercontent.com/d3mondev/0bfff529a4dad627bdb684ad1ef2506d/raw/sponsors.txt"
 )
+
+var GitRevision string
+var GitBranch string
