80a4520688c67e548337d17ea5afd4fa1d683b73
d3mondev <55468528+d3mondev@users.noreply.github.com>
2023-03-27 23:37:42 -0400

    Remove useless test

--- internal/app/cmd/resolve_test.go
+++ internal/app/cmd/resolve_test.go
@@ -1,11 +1,9 @@
 package cmd
 
 import (
-	"os"
 	"testing"
 
 	"github.com/d3mondev/puredns/v2/internal/app/ctx"
-	"github.com/d3mondev/puredns/v2/pkg/filetest"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -24,27 +22,6 @@ func TestRunResolve(t *testing.T) {
 		cmd := newCmdResolve()
 
 		err := runResolve(cmd, []string{"thisfiledoesnotexist.txt"})
-
-		assert.NotNil(t, err)
-	})
-
-	t.Run("with stdin", func(t *testing.T) {
-		r, w, err := os.Pipe()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		err = w.Close()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		filetest.OverrideStdin(t, r)
-
-		context = ctx.NewCtx()
-		cmd := newCmdResolve()
-
-		err = runResolve(cmd, []string{})
 
 		assert.NotNil(t, err)
 	})
